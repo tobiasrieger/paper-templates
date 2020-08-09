930b62720e337324a123675cd8e18894ab105d7c
Tobias <tobias@tobias-macpro.local>
2020-08-09 17:09:40 +0200

    Added files

---
+++ r-markdown-paper/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters"
+    exit -1
+fi
+
+rm -rf .git
+rm README.md
+rm images/*
+rm template.tex
+mv template.rmd $1.rmd
+mv ../r-markdown-paper ../$1
+rm -- "$0"
